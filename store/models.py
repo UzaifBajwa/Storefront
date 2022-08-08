a0f19a6c6416bcf2b6b0eab1654e76ead5219ad9
Uzaif Bajwa <uzaifbajwa11@gmail.com>
2022-08-09 01:04:55 +0500

    Making promotions Optional in products form from product model.

--- store/models.py
+++ store/models.py
@@ -44,7 +44,7 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     # , related_name=product, while default value is product_set
-    promotions = models.ManyToManyField(Promotion)
+    promotions = models.ManyToManyField(Promotion, blank=True)
 
     def __str__(self):
         return self.title
